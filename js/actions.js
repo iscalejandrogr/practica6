658d536aca2cda3221007e07c8604316643c4764
iscalejandrogr <i.s.c.alejandro.gr@gmail.com>
2013-07-12 16:28:25 -0500

    nuevas correcciones

--- js/actions.js
+++ js/actions.js
@@ -1,59 +1,74 @@
-//actions
-var jQT = new $.jQTouch({
-               /* icon: 'jqtouch.png',
-                icon4: 'jqtouch4.png',
-                addGlossToIcon: false,
-                startupScreen: 'jqt_startup.png',
-                statusBar: 'black-translucent',*/
-                themeSelectionSelector: '#jqt'/* #themes ul',
-                preloadImages: []*/
-            });
-$(document).ready(function(e) {
-  document.addEventListener("deviceready", function(){
-    //acelerometro
-    var ace=$('#acelerometro');
-    var watchID=null;
-    ace.find('.individual li').tap(function(){
-      if($(this).index()==0){
-var watchID = navigator.accelerometer.watchAcceleration(function (a) {
-   ace.find('Posición Actual');
-   ace.append('<ul class="post">'+'<li>X'+a.x+'</li>'+'<li>Y'+a.y+'</li>'+'<li>Z'+a.z+'</li>'+'</ul>');
-}, function (err) {
-  alert(err.code);
-}, {Frecuency; 500});
-      
-      }
-      if($(this).index()==1){
-	if(watchID){
-	  ace.find('h2'.text('Detenido'));
-	  ace.find('.pos').remove();
-	  navigator.accelerometer.clearWatch(watchID);
-	  watchID=null;
-	}
-      }
-    });
-    //Brujula
-    var bru=$('#brujula');
-    var watchID=null;
-    bru.find('.individual li').tap(function(){
-      if($(this).index()==0){
-var watchID = navigator.compass.watchHeading(function (h) {
-   bru.find('h2').text('Posición Actual');
-   bru.append('<ul class="post">'+'<li>'+h.magneticHeading+'</li>'+'</ul>');
-}, function (err) {
-  alert(err.code);
-}, {Frecuency; 500});
-      
-      }
-      if($(this).index()==1){
-	if(watchID){
-	  bru.find('h2'.text('Detenido'));
-	  bru.find('.pos').remove();
-	  navigator.compass.clearWatch(watchID);
-	  watchID=null;
-	}
-      }
-    });
-  }, false);
+// JavaScript Document
 
-});
+var jQT = new $.jQTouch({
+                themeSelectionSelector: '#jqt'
+            });
+$(document).ready(function(){
+	document.addEventListener("deviceready",function(){
+		//Acelerometro
+		var ace=$('#acelerometro');
+		var watchID	=null;
+		ace.find('.individual li').tap(function(){
+			if($(this).index()==0)
+			{
+				
+var watchID = navigator.accelerometer.watchAcceleration(function(a){
+	ace.find('h2').text('Posicion Actual');
+	ace.append('<ul class="plastic pos">'+
+	'<li>X: '+a.x+'</li>'+
+	'<li>Y: '+a.y+'</li>'+
+	'<li>Y: '+a.z+'</li>'+
+	'</ul>');
+	}, function(err){
+		alert(err.code);
+		}, {frequency: 500});
+				
+				}
+				
+		if($(this).index()==1)
+		{
+			
+			if(watchID !== null)
+			{
+				ace.find('h2').text('Detenido');
+				ace.find('.pos').remove();
+				navigator.accelerometer.clearWatch(watchID);
+				watchID=null;
+				}
+			}
+			
+			})	
+			
+		//Brujula
+		var bru=$('#brujula');
+		var watchID	=null;
+		bru.find('.individual li').tap(function(){
+			if($(this).index()==0)
+			{
+				
+var watchID = navigator.compass.watchHeading(function(h){
+	bru.find('h2').text('Posicion Actual');
+	bru.append('<ul class="plastic pos">'+
+	'<li>X: '+h.magneticHeading+'</li>'+
+	'</ul>');
+	}, function(err){
+		alert(err.code);
+		}, {frequency: 500});
+				
+				}
+				
+		if($(this).index()==1)
+		{
+			
+			if(watchID !== null)
+			{
+				bru.find('h2').text('Detenido');
+				bru.find('.pos').remove();
+				navigator.compass.clearWatch(watchID);
+				watchID=null;
+				}
+			}
+			
+			})
+		},false);
+		});
